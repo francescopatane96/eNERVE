ecce98431d10833a32a1dc31f979f0d896938307
Francesco Patane, MSc <francesco.patane@live.it>
2023-06-08 15:46:44 +0200

    Update Razor.py

--- code/Razor.py
+++ code/Razor.py
@@ -4,38 +4,7 @@
 import logging
 import os
 
-import os
-import logging
 
-def razor(list_of_proteins, working_dir, transmem_doms_limit, min_loop_length) -> list:
-    " Runs razor module "
-    logging.basicConfig(filename=os.path.join(working_dir, 'logfile.log'),
-                        filemode='a',
-                        level=logging.DEBUG,
-                        force=True)
-
-    for protein in list_of_proteins:
-        if protein.transmembrane_doms >= transmem_doms_limit:
-            raw_loops = protein.provide_raw_loops()
-
-            # Unire i tratti e aggiungere 6 'G' tra di essi
-            longest_loop = 'GGGGGG'.join(raw_loops)
-
-            if longest_loop and len(longest_loop) > min_loop_length:
-                logging.debug(f'Substituting {str(protein.id)} sequence with its longest loop')
-                protein.original_sequence_if_razor = protein.sequence
-                protein.sequence = longest_loop
-                protein.razored = True
-            else:
-                logging.debug(f"No replacement found for {str(protein.id)}")
-                protein.razored = False
-        else:
-            protein.razored = False
-
-    return list_of_proteins
-
-
-'''
 def razor(list_of_proteins, working_dir, transmem_doms_limit, min_loop_length) -> list:
     " Runs razor module "
     logging.basicConfig(filename = os.path.join(working_dir, 'logfile.log'),
@@ -46,12 +15,13 @@ def razor(list_of_proteins, working_dir, transmem_doms_limit, min_loop_length) -
 
     for protein in list_of_proteins:
         if protein.transmembrane_doms >= transmem_doms_limit:
-            longest_loop = max(protein.provide_raw_loops(), key=lambda k: len(k), default=None)
+            new_loop = 'G' * 6 + 'GGGGGG'.join(raw_loops) + 'G' * 6
             
-            if longest_loop and len(longest_loop) > min_loop_length:
-                logging.debug(f'Substituting {str(protein.id)} sequence with its longest loop')
+            
+            if new_loop and len(new_loop) > min_loop_length:
+                logging.debug(f'Substituting {str(protein.id)} sequence with its longest loop'
                 protein.original_sequence_if_razor = protein.sequence
-                protein.sequence = longest_loop
+                protein.sequence = new_loop
                 protein.razored = True
             else:
                 logging.debug(f"No replacement found for {str(protein.id)}")
@@ -61,7 +31,7 @@ def razor(list_of_proteins, working_dir, transmem_doms_limit, min_loop_length) -
     return list_of_proteins
 
 #old version utilized in bNERVE.
-'''
+
 '''
     # logging.debug("Warning: razor utilizes X as an exclusive symbol to split the final protein. Check if X is used inside the protein sequences")
     for p in list_of_proteins:
