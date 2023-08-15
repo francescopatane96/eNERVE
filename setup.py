b2c94bf2418a67cd88201f7e19d3e51947e0c20c
Francesco Patane, MSc <francesco.patane@live.it>
2023-08-15 16:00:30 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enerve',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Lista delle dipendenze necessarie
+    ],
+)
