c57cd40e75819b56866c685a1b9badb81c09c505
Francesco Patane, MSc <francesco.patane@live.it>
2023-08-15 20:49:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-numpy == 1.23.0
-scikit-learn == 1.2.1
-biopython == 1.78
-epitopepredict == 0.5.0
-seaborn == 0.12.2
-pandas == 2.0.1
-requests == 2.31.0
-tensorflow == 2.13.0
-joblib == 1.2.0
-matplotlib == 3.5.0
+    'numpy == 1.23.0',
+    'scikit-learn == 1.2.1',
+    'biopython == 1.78',
+    'epitopepredict == 0.5.0',
+    'seaborn == 0.12.2',
+    'pandas == 2.0.1',
+    'requests == 2.31.0',
+    'tensorflow == 2.13.0',
+    'joblib == 1.2.0',
+    'matplotlib == 3.5.0'
     ],
 )
